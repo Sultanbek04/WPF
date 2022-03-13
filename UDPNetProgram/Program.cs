c24095cb1c041519f301a6fb1a1857e30df91a7e
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:46 +0400

    First commit

---
+++ UDPNetProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPNetProgram
+{
+  class Program
+  {
+    private const string  IP="127.0.0.1";
+    private const int Port=12000;
+    static void Main(string[] args)
+    {
+      UdpClient client=new UdpClient(IP, Port); 
+      string message="Hello World!";
+      byte[] data=Encoding.UTF8.GetBytes(message);
+      int numberOfSentBytes=client.Send(data, data.Length); 
+      client.Close();
+      Console.WriteLine("Good job"); 
+      Console.ReadLine();
+    }
+  }
+}
