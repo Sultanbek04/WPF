4e5833405a13de371d464d3ed73dd360061a3528
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:46 +0400

    First commit

---
+++ ChooseACard/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChooseACard
+{
+  class Program
+  {
+    private const string IP = "127.0.0.1";
+    private const int Port = 12000;
+    static Random random=new Random();
+    static void Main(string[] args)
+    {
+      TcpListener server = null;
+      string response = "";
+      try
+      {
+        server = new TcpListener(IPAddress.Parse(IP), Port);
+        server.Start();
+
+        while (true)
+        {
+          Console.WriteLine("Waiting....");
+          TcpClient client = server.AcceptTcpClient();
+          Console.WriteLine("Connection with Client");
+
+          NetworkStream stream = client.GetStream();
+          byte[] data = new byte[256];
+          var len = stream.Read(data, 0, data.Length);
+          response = Encoding.UTF8.GetString(data, 0, len);
+          int n=random.Next(1, 4);
+
+          if (Convert.ToInt32(response) == n)
+          {
+            
+            byte[] result = Encoding.UTF8.GetBytes("You guessed");
+            stream.Write(result, 0, result.Length);
+            stream.Close();
+            client.Close();
+          }
+          else
+          {
+            byte[] result = Encoding.UTF8.GetBytes("You did not guess");
+            stream.Write(result, 0, result.Length);
+            stream.Close();
+            client.Close();
+          }
+        }
+      }
+       catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+      finally
+      {
+        if (server != null)
+          server.Stop();
+        Console.ReadLine();
+      }
+
+    }
+  }
+}
---
+++ ConsoleServerTCP/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleServerTCP
+{
+  class Program
+  {
+    private const string IP = "127.0.0.1";
+    private const int Port = 12000;
+    static Random random=new Random();
+    static void Main(string[] args)
+    {
+       TcpListener server=null; 
+       
+      try
+      {
+        server = new TcpListener(IPAddress.Parse(IP), Port);  
+        server.Start();
+        while (true)
+        {
+          Console.WriteLine("Waiting...."); 
+          TcpClient client= server.AcceptTcpClient();
+
+          Console.WriteLine("Connection with Client");
+
+
+          NetworkStream stream=client.GetStream();
+          string response=GetNumber();
+
+          byte[] data=Encoding.UTF8.GetBytes(response);
+          stream.Write(data, 0, data.Length); 
+          Console.WriteLine($"Ball on { response}");
+          stream.Close();
+          client.Close();
+        }
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+      finally
+      {
+        if(server!=null)
+           server.Stop();
+        Console.ReadLine(); 
+      }
+    }
+    static string GetNumber()
+    {
+      int val=random.Next(-1, 1+36);
+      if(val==-1)
+        return "00";
+
+      return val.ToString();
+    }
+  }
+}
+    
+  
