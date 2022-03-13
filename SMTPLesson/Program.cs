3dbe69c5745a85101cd34a25a9e1e73aa4ae6016
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:40 +0400

    First commit

---
+++ SMTPLesson/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Mail;
+
+namespace SMTPLesson
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SendMail("msultanbek04@mail.ru", "OpenAI123" , "zhansito@gmail.com");
+    }
+     
+    static void SendMail(string fromMailLogin, string fromMailPassword, string toMailLogin)
+    {
+      MailAddress from=new MailAddress(fromMailLogin, "Бывший король");
+      MailAddress to=new MailAddress(toMailLogin); 
+      MailMessage mail=new MailMessage(from ,to);
+
+      mail.Subject="Мой дорогой друг"; 
+
+      mail.Body="<h3>  Мой дорогой друг </h3> <p/> Пожалуйста примите мои извинения за отпарвку вам этого " +
+        "письма без вашего согласия"; 
+
+      mail.IsBodyHtml=true;
+
+      SmtpClient smtp=new SmtpClient("smtp.mail.ru", 2525);
+      smtp.Credentials=new NetworkCredential(fromMailLogin, fromMailPassword);
+      smtp.EnableSsl=true;
+      smtp.Send(mail);  
+
+        
+    }
+  } 
+}
