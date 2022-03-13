42f8079b3999b8c95e0fcc6f0106949f266c8a10
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:45 +0400

    First commit

---
+++ FTPHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace FTP
+{
+
+  class Program
+  {
+    const string ftp = "ftp://10.16.13.119/";
+    const string DirectoryName = "TestFTP";
+    
+
+    static void Main(string[] args)
+    {
+      FtpWebRequest requestCreate = (FtpWebRequest)FtpWebRequest.Create(ftp + DirectoryName);
+      requestCreate.KeepAlive = false;
+      Console.WriteLine("Getting the response");
+      requestCreate.Method = WebRequestMethods.Ftp.MakeDirectory;   
+      using (var resp = (FtpWebResponse)requestCreate.GetResponse())
+      {
+        Console.WriteLine(resp.StatusCode);
+      }
+
+
+      FtpWebRequest requestDelete = (FtpWebRequest)FtpWebRequest.Create(ftp + DirectoryName);
+      requestDelete.KeepAlive = false;
+      Console.WriteLine("Getting the response");
+      requestDelete.Method = WebRequestMethods.Ftp.RemoveDirectory;
+      using (var resp = (FtpWebResponse)requestDelete.GetResponse())
+      {
+        Console.WriteLine(resp.StatusCode);
+      }
+
+
+      Console.ReadLine();
+
+    }
+  }
+}
