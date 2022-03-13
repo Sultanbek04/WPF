e6a98e4422e19015e0370b3707a10dd75ff486c3
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:44 +0400

    First commit

---
+++ HttpListener_SelfStudy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace HttpListener_SelfStudy
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     HttpListener httpListener=new HttpListener();
+      AuthenticationSchemes authenticationSchemes=new AuthenticationSchemes();
+      authenticationSchemes.
+      httpListener.AuthenticationSchemes=
+
+
+    }
+  }
+}
