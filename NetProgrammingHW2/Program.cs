d29556bc2ea945ec795a3e4e7a965b40f900c499
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:44 +0400

    First commit

---
+++ NetProgrammingHW2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace NetProgrammingHW2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Please write a domen of site: ");    
+      var host=Dns.GetHostEntry(Console.ReadLine());
+      foreach (IPAddress iPAddress in host.AddressList)
+      {
+        Console.WriteLine(iPAddress.ToString());
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
