e0b4dd6afe91417e1acd553b88d528b48ac66d63
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:54:44 +0400

    First commit

---
+++ HTTP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;  
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace HTTP
+{
+  class Program
+  {
+    private const string IP="127.0.0.1";
+    private const int Port=12000;
+    static void Main(string[] args)
+    {
+      WebClient client=new WebClient();
+       client.DownloadFile("https://translate.google.kz/", "googleT.html"); 
+      Console.WriteLine("Файл загружен"); 
+
+    }
+  }
+}
